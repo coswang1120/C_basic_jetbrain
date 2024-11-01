f14b9d48459e1d5c5251e46ae01522b8d3c1bd82
coswang1120 <coswang1120@gmail.com>
2024-11-01 21:39:28 +0800

    新增泡泡排序與打印數組函數
    添加泡泡排序功能到main.c，包括`bubbleSort`與`printArray`函數，並在主函數中整合這些操作。更新readme.md記錄此變更,新增bubble_sort_functions.md文件詳細記錄此修改。

--- main.c
+++ main.c
@@ -1,29 +1,50 @@
-// main.c
-
 #include <stdio.h>
 
-// 主函數，程式的入口點
+/**
+ * 使用泡泡排序算法對數組進行排序
+ *
+ * @param arr 待排序數組
+ * @param n 數組的長度
+ */
+void bubbleSort(int arr[], int n)
+{
+    int i, j, temp;
+    for (i = 0; i < n-1; i++)  // 外層迴圈：控制遍歷次數
+    {
+        for (j = 0; j < n-i-1; j++)  // 內層迴圈：比較相鄰元素並交換
+        {
+            if (arr[j] > arr[j+1])  // 如果前一個元素大於後一個元素，則交換
+            {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
+
+/**
+ * 打印數組
+ *
+ * @param arr 數組
+ * @param size 數組的長度
+ */
+void printArray(int arr[], int size)
+{
+    int i;
+    for (i = 0; i < size; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
 int main()
 {
-    // 宣告並初始化變數 a，賦值為 100
-    int a = 100;
-
-    // 判斷變數 a 是否小於 20
-    if(a < 20)
-    {
-        // 如果 a 小於 20，則輸出 "a < 20"
-        printf("a < 20\n");
-    }
-    else
-    {
-        // 如果 a 不小於 20，則輸出 "a 大於 20" 兩次
-        printf("a 大於 20\n");
-        printf("a 大於 20\n");
-    }
-
-    // 輸出變數 a 的值
-    printf("a 等於 %d\n", a);
-
-    // 程式結束，返回 0
+    int arr[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    printf("排序前的數組: \n");
+    printArray(arr, n);
+    bubbleSort(arr, n);
+    printf("排序後的數組: \n");
+    printArray(arr, n);
     return 0;
 }
